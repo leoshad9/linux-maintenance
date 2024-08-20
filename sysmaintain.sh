f230f70cd46302aea22207c5cc16bd38107cf48b
Mohammad Shadman <leoshad9@gmail.com>
2024-08-20 15:04:21 +0530

    Update

--- sysmaintain.sh
+++ sysmaintain.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-# Auhtor: Mohammad Shadman
+# Author: Mohammad Shadman
 
-# It will update & clean the system.  
+# It will update & clean the system.
 
-echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting\n"
+echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting system update and cleanup\n"
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade 
-sudo apt-get -y autoremove
-sudo apt-get -y clean
-sudo apt-get -y autoclean
+# Array of commands for update and cleanup
+COMMANDS=(
+    "sudo apt-get -y update"
+    "sudo apt-get -y upgrade"
+    "sudo apt-get -y dist-upgrade"
+    "sudo apt-get -y autoremove"
+    "sudo apt-get -y clean"
+    "sudo apt-get -y autoclean"
+)
 
-echo -e "\n$(date "+%d-%m-%Y --- %T") --- Done"
+# Execute each command and check for errors
+for cmd in "${COMMANDS[@]}"; do
+    echo "Running: $cmd"
+    if $cmd; then
+        echo "Completed: $cmd"
+    else
+        echo "Error: Command failed - $cmd"
+        exit 1
+    fi
+done
+
+echo -e "\n$(date "+%d-%m-%Y --- %T") --- System update and cleanup completed"
